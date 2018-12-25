d46b6d179f4168edd43444f2691dd993f1754af0
guanqiao1016 <708805486@qq.com>
2018-12-25 15:18:40 +0800

    change

--- src/pages/home/compenents/swiper.vue
+++ src/pages/home/compenents/swiper.vue
@@ -3,8 +3,6 @@
     <swiper :options="swiperOption">
       <!-- slides -->
       <swiper-slide v-for="item of swiperList" :key="item.id" ><img  class="swiper-img" :src="item.imgUrl"/></swiper-slide>
-      <swiper-slide ><img  class="swiper-img" src=""/></swiper-slide>
-      <swiper-slide ><img  class="swiper-img" src=""/></swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
