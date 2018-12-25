3d60d15481f3d44c5a61b659e4283895c7375c80
guanqiao1016 <708805486@qq.com>
2018-12-25 15:44:46 +0800

    change

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,16 +2,18 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 <script>
 import homeHeader from './compenents/header'
 import homeSwiper from './compenents/swiper'
+
 export default {
   name: 'home',
   components: {
     homeHeader,
-    homeSwiper
+    homeSwiper,
   }
 }
 </script>
