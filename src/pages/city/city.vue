d7a800a9b8198d22f4d28ed3b29794ad592322d3
guanqiao1016 <708805486@qq.com>
2018-12-26 21:16:13 +0800

    city-search

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,13 +1,17 @@
 <template>
-  <city-header></city-header>
-
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import cityHeader from './compenents/header'
+import citySearch from './compenents/search'
 export default {
   name: 'city',
   components:{
     cityHeader,
+    citySearch
   }
 }
 </script>
---
+++ src/pages/city/compenents/search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input class="search-input"  type="text" placeholder="请输入城市名或拼音"/>
+  </div>
+</template>
+<script>
+export default {
+  name:'citySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles'
+  .search
+      height:.72rem
+      background:$bgColor
+      padding:0 .1rem
+    .search-input
+      height:.62rem
+      width:100%
+      line-height:.62rem
+      text-align:center
+      border-radius:.06rem
+      color:#666
+
+
+</style>
